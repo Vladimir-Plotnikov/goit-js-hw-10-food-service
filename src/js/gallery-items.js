4182bcb9d9300f423ccb2bcc934b6a947c09e03f
Vladimir-Plotnikov <71009993+Vladimir-Plotnikov@users.noreply.github.com>
2021-03-23 14:54:46 +0200

    Last Commits
    TheLast

--- src/js/gallery-items.js
+++ src/js/gallery-items.js
@@ -1,5 +1,39 @@
-// import galleryTpl from '../templates/gallery-items.hbs'
-// console.log(galleryTpl);
-
+import galleryTpl from '../templates/gallery-items.hbs'
 import galleryMenu from '../menu.json'
-console.log(galleryMenu);
+
+
+const theme = {
+  light: 'light-theme',
+  dark: 'dark-theme',
+};
+
+saveDataCheck();
+
+const switcher = document.querySelector('.theme-switch__toggle')
+const menuContainer = document.querySelector('.menu')
+const cardsMarkup = createCardsMarkup(galleryMenu)
+menuContainer.insertAdjacentHTML('beforeend', cardsMarkup)
+
+function createCardsMarkup(galleryMenu) {
+    return galleryMenu.map(galleryTpl).join('');
+}
+
+
+switcher.onclick = () => {
+    if(switcher.checked) {
+        localStorage.setItem('theme', theme.dark)
+        document.body.classList.toggle(theme.dark)
+        return
+    }
+    localStorage.setItem('theme', theme.light)
+    document.body.classList.remove(theme.dark)
+}
+
+function saveDataCheck() {
+    const saveData = localStorage.getItem('theme')
+    if (saveData) {
+        console.log(saveData);
+        document.body.classList.add(saveData);
+    }
+}
+
